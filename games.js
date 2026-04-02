de97926e051dc3b784c483d2adbfcc849a516ffd
phoenix <phoenix.j.anthony@msd549c.org>
2026-04-02 15:29:54 -0700

    Create games.js file with games database array

---
+++ games.js
@@ -0,0 +1,8 @@
+const games = [
+    { id: 1, title: 'Chess', genre: 'Strategy' },
+    { id: 2, title: 'Super Mario', genre: 'Platform' },
+    { id: 3, title: 'Call of Duty', genre: 'Shooter' },
+    { id: 4, title: 'The Legend of Zelda', genre: 'Adventure' }
+];
+
+module.exports = games;
